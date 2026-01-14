1ababb3c8aa94993aca5f3df8d9c7cb91a79e95b
unknown <intanramadanii88@gmail.com>
2026-01-15 03:27:08 +0700

    push backend2

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,9 @@
 <?php
-define('DB_HOST', 'localhost'); 
+define('DB_HOST', 'interchange.proxy.rlwy.net'); 
 define('DB_USER', 'root');      
-define('DB_PASS', '');          
-define('DB_NAME', 'db_ecommerce'); 
+define('DB_PASS', 'hJHRdMpBByXMiBbMhuWNOmjKLYnWijfu');          
+define('DB_NAME', 'railway'); 
+define('DB_PORT', 42188);
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
